497f057e770b682d874c2cb5dc16bd3baa6fec0b
Pierdet <antemxz@hotmail.com>
2020-06-30 21:20:08 +0200

    Implemented method for parallel host validation

--- Cim.Lib/Net/ConnectionValidator.cs
+++ Cim.Lib/Net/ConnectionValidator.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Cim.Lib.Net
 {
@@ -46,7 +47,49 @@ namespace Cim.Lib.Net
                 };
             }
         }
+        private async Task<ConnectionValidationResponse> ValidateAsync(string hostName)
+        {
+            try
+            {
+                var reply = await _pinger.SendPingAsync(hostName, 1000);
 
+                if (reply == null)
+                {
+                    return new ConnectionValidationResponse
+                    {
+                        Connection = hostName,
+                        Success = false,
+                        ErrorMessage = "could not receive a reply"
+                    };
+                }
+
+                return new ConnectionValidationResponse
+                {
+                    Connection = hostName,
+                    Success = reply.Status == IPStatus.Success
+                };
+            }
+            catch (Exception e)
+            {
+                return new ConnectionValidationResponse
+                {
+                    Connection = hostName,
+                    Success = false,
+                    ErrorMessage = $"unexpected error occurred: {e.Message}"
+                };
+            }
+        }
+
+        public async Task<IEnumerable<ConnectionValidationResponse>> ValidateParallelAsync(List<string> hostNames)
+        {
+            var tasks = new List<Task<ConnectionValidationResponse>>();
+            foreach (var hostName in hostNames)
+            {
+                tasks.Add(ValidateAsync(hostName));
+            }
+            var result = await Task.WhenAll(tasks);
+            return result;
+        }
         public void Dispose()
         {
             _pinger.Dispose();
--- Cim.Lib/Net/IConnectionValidator.cs
+++ Cim.Lib/Net/IConnectionValidator.cs
@@ -2,12 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Cim.Lib.Net
 {
     public interface IConnectionValidator
     {
         public ConnectionValidationResponse Validate(string hostName);
-        //Todo Implement ValidateParallel
+        public Task<IEnumerable<ConnectionValidationResponse>> ValidateParallelAsync(List<string> hostNames);
     }
 }
